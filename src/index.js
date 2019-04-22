bd4543398ebf30c6d1476267ac353efc83a60a91
wzht1991 <506486189@qq.com>
2019-04-22 17:58:57 +1000

    get data by using axios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express=require('express');
+const axios=require('axios');
 const app=express();
 app.get("/",(req,res)=>{
     res.send("welcome");
 });
-app.get('/weather/:cc/:city',(req,res)=>{
-    res.send("weather");
+app.get('/weather/:city/:country',(req,res)=>{
+    const {city,country}=req.params;
+    axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=70524de0cb2bf075bed8d2716ac3d222&units=metric`
+    ).then(Response=>{
+         res.send(Response.data);
+    }).catch(err=>{
+        res.send(err);
+    })
+   
 })
 app.listen(3000,()=>{
     console.log("welcome port 3000");
