abd6bdac8d5092a5345b0efbb8e9bee621ed892d
wzht1991 <506486189@qq.com>
2019-04-30 17:39:07 +1000

    fix Access-Control-Allow-Origin problem?

---
+++ src/access.js
@@ -0,0 +1,9 @@
+// 解决了跨域问题？
+const express=require('express');
+const app=express();
+   module.exports=app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 const express=require('express')
 const weathercurrent=require('./routers/weathercurrent');
 const router=express.Router();
-
+const access= require('./access');
 router.get("/",(req,res)=>{
     res.send("welcome");
  });
-router.use('/weather/current',weathercurrent);
+router.use('/weather',access,weathercurrent);
 module.exports=router;
